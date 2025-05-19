0010364bde134f5dd3e1230e2b1af3c8d58923a4
viczveras <victor@DESKTOP-H81PMV3.>
2025-05-19 19:42:12 -0300

    teste

---
+++ app.py
@@ -0,0 +1,21 @@
+# questão 1 
+
+
+print('Hello World')
+
+
+# questão 2 
+
+
+num1 = int(input('Digite o primeiro  número: '))
+num2 = int(input('Digite o segundo n numero'))
+
+
+soma = num1 + num2 
+
+print(f'A soma de {num1} + {num2} é {soma}')
+
+
+# questão 3 
+
+
