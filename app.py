38b5d18a998f602e9e0e9ac26dc8fc16d36fe7ca
viczveras <viczveras1@gmail.com>
2025-05-21 13:05:14 -0300

    ending pratica 1

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ print('Hello World')
 # questão 2 
 
 
-num1 = int(input('Digite o primeiro  número: '))
-num2 = int(input('Digite o segundo n numero'))
+num1 = 12
+num2 = 14
 
 
 soma = num1 + num2 
@@ -24,4 +24,31 @@ largura = 10
 altura = 20 
 
 volume = comprimento * largura * altura
-print(f'Calculdo  do volume = {volume}')
+print(f'Calculdo  do volume = {volume}cm³')
+
+# questão 4 
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("4- Calculadora de Preço Total")
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total:.2f}")
+print("-" * 40)
+
+# questao 5
+print("5- Diferença de Produtos")
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+diferenca = (A * B) - (C * D)
+
+print(f"DIFERENCA = {diferenca}")
+print("-" * 40)
